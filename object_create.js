19a3e21afae6fda8c36f1373931426dd2b6e4318
shadab057 <mohdshadab4629@gmail.com>
2025-03-12 14:28:13 +0530

    Object.create method part-2

--- object_create.js
+++ object_create.js
@@ -1,9 +1,19 @@
-const person = {
-    greet: function() {
-      console.log("Hello!");
-    }
-  };
-  
-  const student = Object.create(person);
-  student.greet(); // Output: Hello!
-  
+// 
+
+
+let vehicle = {
+  wheels: '4',
+  fuelType: 'Gasoline',
+  color: 'Green'
+}
+let carProps = {
+  type: {
+    value: 'Volkswagen'
+  },
+  model: {
+    value: 'Golf'
+  }
+}
+
+var car = Object.create(vehicle, carProps);
+console.log(car);
