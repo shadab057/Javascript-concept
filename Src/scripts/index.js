f898c7a4ee08e19b7ad9378df2f2561bf57b1abc
shadab057 <mohdshadab4629@gmail.com>
2025-04-10 23:52:35 +0530

    DOM hierarchy JavaScript takes control over HTML elements

--- Src/scripts/index.js
+++ Src/scripts/index.js
@@ -1,6 +1,6 @@
-<!--
-        "use strict";
-        function printpage() {
-            window.print();
-        }
-        -->>
+function bodyload() {
+    document.images[0].src = "/public/images/photo.jpg";
+    document.images[0].width = 300;
+    window.document.forms[0].elements[1].value = "Register"
+    window.document.forms[1].elements[1].value = "Login";
+}
