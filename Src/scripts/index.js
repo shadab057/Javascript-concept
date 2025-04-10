71cada5c6c6c1116e3fe1101909db0d88f35255b
shadab057 <mohdshadab4629@gmail.com>
2025-04-11 00:54:21 +0530

    JavaScript can refer elements by using "name"

--- Src/scripts/index.js
+++ Src/scripts/index.js
@@ -1,6 +1,3 @@
-function bodyload() {
-    document.images[0].src = "/public/images/photo.jpg";
-    document.images[0].width = 300;
-    window.document.forms[0].elements[1].value = "Register"
-    window.document.forms[1].elements[1].value = "Login";
+function bodyload(){
+    pic.src = "/public/images/corner.jpg"
 }
