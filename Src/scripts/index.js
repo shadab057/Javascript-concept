c669dfe125eb20a2477166fe6a4de9a45c1ab44d
shadab057 <mohdshadab4629@gmail.com>
2025-04-11 00:58:16 +0530

    can't access any child element directly

--- Src/scripts/index.js
+++ Src/scripts/index.js
@@ -1,3 +1,6 @@
 function bodyload(){
     pic.src = "/public/images/corner.jpg"
+    pic.src = width =200;
+    frmRegister.btnRegister.value = "Register";
+    frmLogin.btnLogin.value = "Login";
 }
